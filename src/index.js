615477f7f5ef9927bbaaa2f281dd421734ea61f6
Fabian Hanso <f.hanso@revice-media.com>
2023-02-01 16:21:24 +0100

    delete dependencies

--- src/index.js
+++ src/index.js
@@ -2,11 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <Provider>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
